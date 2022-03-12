548049936628070c3afd5a3274b993cba562484f
mamadou lamine ba <laminba2003@gmail.com>
2022-03-12 21:27:54 +0100

    initial commit

--- src/com/training/concurrency/ScheduledThreadPool.java
+++ src/com/training/concurrency/ScheduledThreadPool.java
@@ -1,6 +1,5 @@
 package com.training.concurrency;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
